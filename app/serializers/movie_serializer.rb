c915a847be9b84c452131d0ebdf403093a24187a
Njoki <kinuthiash@gmail.com>
2022-07-27 15:19:48 +0300

    commit tree b48640a48f1a0828af5c2aa74deff73f75063f3d

--- app/serializers/movie_serializer.rb
+++ app/serializers/movie_serializer.rb
@@ -1,4 +1,6 @@
 class MovieSerializer < ActiveModel::Serializer
-  attributes :id, :title, :year, :length, :description, :poster_url, :category, :discount
+  attributes :id, :title, :year, :length,   :category, :discount
   
+  belongs_to :director
+  has_many :reviews
 end
